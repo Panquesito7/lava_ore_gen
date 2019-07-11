ef72058ac327e16a40ea6eb76a1a14ca0ebb0bb6
Coder12a <38924418+Coder12a@users.noreply.github.com>
2019-07-11 12:14:49 -0500

    Remove: default depend

--- init.lua
+++ init.lua
@@ -1,12 +1,17 @@
+local stone_name = minetest.settings:get("lava_ore_gen.stone_name") or "default:stone"
 local interval = tonumber(minetest.settings:get("lava_ore_gen.interval")) or 20
 local chance = tonumber(minetest.settings:get("lava_ore_gen.chance")) or 3600
 local random = minetest.settings:get_bool("lava_ore_gen.random") or false
 
 local function create_hotstone()
+	minetest.register_node("lava_ore_gen:stone_hot", {name = "lava_ore_gen:stone_hot"})
+end
+local function override_hotstone()
 	local clonenodeForLava = {}
-	for k, v in pairs(minetest.registered_nodes["default:stone"]) do clonenodeForLava[k] = v end
-	clonenodeForLava.groups.not_in_creative_inventory = 1
+	for k, v in pairs(minetest.registered_nodes[stone_name]) do clonenodeForLava[k] = v end
+	clonenodeForLava.name = "lava_ore_gen:stone_hot"
 	clonenodeForLava.description = "Heated Stone"
+	clonenodeForLava.groups.not_in_creative_inventory = 1
 	clonenodeForLava.tiles = {"default_stone.png^[colorize:red:20"}
 	clonenodeForLava.paramtype = "light"
 	clonenodeForLava.light_source = 4
@@ -24,7 +29,7 @@ local function create_hotstone()
 					end
 				end
 				-- Do math to pick a ore.
-				local ore = {rarity = -1, name = "default:stone"}
+				local ore = {rarity = -1, name = stone_name}
 				for i, v in next, ore_map do
 					if ore.rarity == -1 or ore.rarity > math.random(0, v.rarity) then
 						ore = v
@@ -32,18 +37,22 @@ local function create_hotstone()
 				end
 				minetest.set_node(pos, {name = ore.name})
 			else
-				minetest.set_node(pos, {name = minetest.registered_ores[math.random(1, #minetest.registered_ores + 1)]})
+				local ore_map = {}
+				for i, v in next, minetest.registered_ores do
+					ore_map[#ore_map + 1] = v.ore
+				end
+				minetest.set_node(pos, {name = ore_map[math.random(1, #ore_map + 1)]})
 			end
 		else
-			minetest.set_node(pos, {name = "default:stone"})
+			minetest.set_node(pos, {name = stone_name})
 		end
 		return true
 	end
-	minetest.register_node("lava_ore_gen:stone_hot", clonenodeForLava)
+	minetest.register_node(":lava_ore_gen:stone_hot", clonenodeForLava)
 end
 local function override_stone()
 	-- make stone floodable --
-	minetest.override_item("default:stone", {
+	minetest.override_item(stone_name, {
 		floodable = true,
 		on_flood = function(pos, oldnode, newnode)
 			local def = minetest.registered_items[newnode.name]
@@ -61,4 +70,7 @@ local function override_stone()
 end
 
 create_hotstone()
-override_stone()
+minetest.register_on_mods_loaded(function()
+	override_hotstone()
+	override_stone()
+end)
